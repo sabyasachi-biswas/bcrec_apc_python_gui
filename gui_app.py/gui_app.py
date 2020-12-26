333d68146b3b63b639e6c5b20540e8dc2293d2b1
Sabyasachi Biswas <49406254+sabyasachi-biswas@users.noreply.github.com>
2020-12-26 20:02:40 +0530

    Update

--- gui_app.py/gui_app.py
+++ gui_app.py/gui_app.py
@@ -4,12 +4,15 @@ def printName(event):
     print("Sabyasachi Biswas")
 
 root = Tk()
+root.title("GUI app")
+canvas = Canvas(root, width = 450 , height = 500)
+canvas.pack()
 label1 = Label(root, text = "Hello World", fg = "red")
 label1.pack(side = TOP)
 topFrame = Frame(root)
 topFrame.pack()
 
-button1 = Button(topFrame, text = "Click", fg = "green", bg = "cyan")
+button1 = Button(canvas, text = "Click", fg = "green", bg = "cyan")
 button1.pack(side = LEFT)
 button2 = Button(topFrame, text = "Click", fg = "red", bg = "cyan")
 button2.pack(side = RIGHT)
