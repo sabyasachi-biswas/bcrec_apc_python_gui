0394ac410906674b5dc3e1888a816ad0a261742e
Sabyasachi Biswas <49406254+sabyasachi-biswas@users.noreply.github.com>
2020-12-26 19:48:15 +0530

    Update gui_app.py

--- gui_app.py/gui_app.py
+++ gui_app.py/gui_app.py
@@ -1,5 +1,8 @@
 from tkinter import *
 
+def printName(event):
+    print("Sabyasachi Biswas")
+
 root = Tk()
 label1 = Label(root, text = "Hello World", fg = "red")
 label1.pack(side = TOP)
@@ -12,8 +15,13 @@ button2 = Button(topFrame, text = "Click", fg = "red", bg = "cyan")
 button2.pack(side = RIGHT)
 button3 = Button(topFrame, text = "Click", fg = "grey", bg = "cyan")
 button3.pack(side = BOTTOM)
-button4 = Button(topFrame, text = "Click", fg = "black", bg = "cyan")
+
+
+button4 = Button(topFrame, text = "Four", fg = "black", bg = "cyan")
+button4.bind("<Button-1>", printName)
 button4.pack(side = BOTTOM)
+
+
 
 label1 = Label(root, text = "Hello World", fg = "red")
 label1.pack()
